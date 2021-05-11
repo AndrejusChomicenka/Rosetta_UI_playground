5781623a6d8fd04904f86ddb40290d2e00b80377
achomicenka <andrejus.chomicenka@bazaarvoice.com>
2021-05-11 16:35:46 +0300

    basic rosetta call for prod and stg comparison

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# Rosetta_UI_playground
--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const [callProd, setCallProd] = useState('')
+  const [callStg, setCallStg] = useState('')
+  const [client, setClient] = useState('')
+
+  const callHandler = async (e) => {
+    e.preventDefault()
+    const getResponseProd = async () => {
+      const resProd = await fetch(
+        `https://rosetta.prod.us-east-1.nexus.bazaarvoice.com/package/1/client/${client}`
+      )
+      const dataProd = await resProd.text()
+      setCallProd(dataProd)
+    }
+    
+    const getResponseStg = async () => {
+      const resStg = await fetch(
+        `https://rosetta-stg.prod.us-east-1.nexus.bazaarvoice.com/package/1/client/${client}`
+        )
+      const dataStg = await resStg.text()
+      setCallStg(dataStg)
+    }
+      
+    const getAll = async () => {
+      await getResponseProd()      
+      await getResponseStg()
+    }
+
+    getAll()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form>
+        <input
+          placeholder='Client'
+          value={client}
+          onChange={(event) => setClient(event.target.value)}
+        />
+        <button
+          type='submit'
+          onClick={callHandler}
         >
-          Learn React
-        </a>
-      </header>
+          CALL
+        </button>
+      </form>
+      <div className='container'>
+        <div className='prod'>{callProd}</div>
+        <div className='stg'>{callStg}</div>
+      </div>
     </div>
   );
 }
